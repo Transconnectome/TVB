3dcdcb464169ccc6525e8c23d1b70a1e3a1698ef
dyhan316 <dyhan0316@gmail.com>
2022-07-26 20:00:17 +0900

    checking

---
+++ jungwoos_code/BNM_calculation_function.py
@@ -0,0 +1,280 @@
+# import relavent package
+import sys
+sys.path.append('/usr/local/lib/python3.8/dist-packages/')
+
+import numpy as np
+import pandas as pd
+import bct
+import time
+import math
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+########################
+### define functions ###
+########################
+
+# connectivity vector form data --> matrix form
+def from_vec_form_to_mat_form(input_data, n_node):
+    # input : connectivity vector form data
+    # output : connectivity matrix form data
+
+    init = time.time()
+
+    count_mat_data = np.zeros((len(input_data), n_node, n_node))
+    connectivity_matrix = np.zeros((n_node, n_node))
+    for sub in range(len(input_data)):
+        loc = 0
+        for j in range(n_node):
+            connectivity_matrix[j, j+1:] = input_data.iloc[sub, loc:(loc + (n_node - 1) - j)]
+            loc += (n_node - 1) - j
+        count_mat_data[sub] = connectivity_matrix + connectivity_matrix.T
+
+    print(f'vector form --> matrix form conversion finish, time = {time.time() - init}')
+    return count_mat_data     # type of this matrix is 3-dim np.array
+
+
+# matrix form connectivity data --> vector form data
+def from_mat_form_to_vec_form(input_data):
+    connectivity_line_form_data = np.zeros(int(len(input_data) * (len(input_data)-1) / 2))
+    loc = 0
+    for j in range(len(input_data)):
+        connectivity_line_form_data[loc:(loc+len(input_data) - 1 - j)] = input_data[j, j + 1:]
+        loc += len(input_data) - 1 -j
+    return connectivity_line_form_data
+
+
+def weight_based_threshold(input_data, threshold: int):
+    # remove connection when streamline count below the threshold
+    # math :: if streamline count <= threshold : streamline counts = 0
+
+    init = time.time()
+
+    thresholded_data = input_data.copy()
+    condition = thresholded_data <= threshold
+    thresholded_data[condition] = 0
+
+    print(f'weight-based thresholding is finished, time = {time.time() - init}')
+    return thresholded_data
+
+def density_based_threshold(input_data, threshold):
+    # making the connection matrix to have density = threshold
+    # input : connectivity matrix
+    # output : thresholded connectivity matrix
+
+    init = time.time()
+
+    thresholded_data = np.zeros(input_data.shape)
+    for i in range(len(input_data)):
+        thresholded_data[i] = bct.threshold_proportional(input_data[i], threshold)
+    print(f'density based thresholding is finished, time = {time.time() - init}')
+
+    return thresholded_data
+
+
+def calcul_density(input_data):
+    # input data : connectivity "matrix"
+    # output data : density
+
+    init = time.time()
+
+    density = np.zeros(len(input_data))
+    for i in range(len(input_data)):
+        density[i], b, c = bct.density_und(input_data[i])
+    print(f'density calculation is finished, time = {time.time() - init}')
+    return density
+
+def calcul_n_comp(input_data):
+    # input : connectivity matrix
+    # output : number of components of the network
+
+    init = time.time()
+
+    n_comp = np.zeros(len(input_data))
+    for i in range(len(input_data)):
+        n_comp[i] = len(set(bct.get_components(input_data[i])[0]))
+    print(f'the number of network component calculation is finished, time = {time.time() - init}')
+    return n_comp
+
+def calcul_connection_length_mat(input_data):
+    # input : connectivity matrix
+    # output : connection length matrix
+    # math : connection length  = 1 / connection weight
+
+    init = time.time()
+
+    connection_length_mat = np.zeros(input_data.shape)
+    for i in range(len(input_data)):
+        connection_length_mat[i] = bct.weight_conversion(input_data[i], 'lengths')
+
+    print(f'connection length matrix calculation is finished, time = {time.time() - init}')
+    return connection_length_mat
+
+
+def calcul_distance_mat(connection_length_mat):
+    # input : connection length matrix
+    # output : distance matrix, number of edges in shortest path (NOE_in_SP)
+
+    init = time.time()
+
+    distance_mat = np.zeros(connection_length_mat.shape)
+    NOE_in_SP = np.zeros(connection_length_mat.shape)
+    for i in range(len(connection_length_mat)):
+        distance_mat[i], NOE_in_SP[i] = bct.distance_wei(connection_length_mat[i])
+
+    print(f'distance matrix calculation is finished, time = {time.time() - init}')
+
+    return distance_mat, NOE_in_SP
+
+
+def make_nodal_column_list(region_list, variable_name):
+    column_name_list = region_list.copy()
+    for i in range(len(region_list)):
+        column_name_list[i] = variable_name + '_' + region_list[i]
+    return column_name_list
+
+def calcul_degree(input_data, n_node):
+    # input : connectivity matrix
+    # output : degree
+    init = time.time()
+    degree = np.zeros((len(input_data),n_node))
+    for i in range(len(input_data)):
+        degree[i] = bct.degrees_und(input_data[i])
+    print(f'degree calculation is finished, time = {time.time() - init}')
+    return degree
+
+def calcul_strength(input_data, n_node):
+    # input : connectivity matrix
+    # output : strength
+    init = time.time()
+    strength = np.zeros((len(input_data),n_node))
+    for i in range(len(input_data)):
+        strength[i] = bct.strengths_und(input_data[i])
+    print(f'strength calculation is finished, time = {time.time() - init}')
+    return strength
+
+def calcul_global_efficiency(distance_mat):
+    # input : distance matrix
+    # output : global efficiency of the network
+
+    init = time.time()
+    efficiency = np.zeros(len(distance_mat))
+    for i in range(len(distance_mat)):
+        a, efficiency[i], c, d, e = bct.charpath(distance_mat[i])
+    print(f'global efficiency calculation is finished, time = {time.time() - init}')
+    return efficiency
+
+def calcul_clust_coef(input_data, n_node):
+    # input : connectivity matrix
+    # output : clustering coefficient
+    init_1 = time.time()
+    clustering_coefs = np.zeros((len(input_data), n_node))
+    for i in range(len(input_data)):
+        clustering_coefs[i] = bct.clustering_coef_wu(input_data[i])
+    print('clustering_coef calculation time = %f' % (time.time() - init_1))
+    return clustering_coefs
+
+
+def calcul_module_and_modularity_Louvain(input_data, n_node):
+    init_1 = time.time()
+
+    modular_structures = np.zeros((len(input_data), n_node))
+    modularities = np.zeros(len(input_data))
+    for i in range(len(input_data)):
+        j = 0
+
+        # to constraint the number of modules ~ 5
+        while modular_structures[i].max() != 5:
+            modular_structures[i], modularities[i] = bct.community_louvain(input_data[i])
+            j += 1
+            if j > 10000:
+                if (modular_structures[i].max() == 6) or (modular_structures[i].max() == 4):
+                    break
+    print('modular structure calculation time = %f' %(time.time() - init_1))
+    return modular_structures, modularities
+
+def calcul_s_core(input_data, n_node):
+    # input : connectivity matrix
+    # output : s-core index of the nodes (and if you want s-core size also)
+    init = time.time()
+    s_core_index = np.zeros((len(input_data), n_node))
+    s_core_size = np.zeros((len(input_data), 100))
+    for sub_num in range(len(input_data)):
+        s_core_index_list = np.zeros(n_node)
+        score_size_list = np.zeros(100)
+        for i in range(100):
+            ss, score_size_list[i] = bct.score_wu(input_data[sub_num], 0.01 * i)
+            cond = ss.sum(axis=1) != 0
+            s_core_index_list[cond] = 0.01 * i
+
+        s_core_index[sub_num] = s_core_index_list
+        s_core_size[sub_num] = score_size_list
+    print(f's-core index calculation is finished, time = {time.time() - init}')
+    return s_core_index, s_core_size
+
+def calcul_k_core(input_data, n_node):
+    # input : connectivity matrix
+    # output : k-core index
+    init = time.time()
+    kcore_index = np.zeros((len(input_data), n_node))
+    for i in range(len(input_data)):
+        kcore_index[i], b = bct.kcoreness_centrality_bu(np.sign(input_data[i]))
+    print(f'k-core index calculation is finished, time = {time.time() - init}')
+    return kcore_index
+
+def calcul_closeness_centrality(distance_mat, n_node):
+    # input : distance_matrix
+    # output : closeness centrality = nodal efficiency
+    init = time.time()
+    closeness_centrality = np.zeros((len(distance_mat), n_node))
+    for i in range(len(distance_mat)):
+        for j in range(n_node):
+            for k in range(n_node):
+                if j != k:
+                    closeness_centrality[i][j] += 1 / distance_mat[i][j][k]
+            closeness_centrality[i][j] /= (n_node - 1)
+    print('time consumed in calculating Cc, %f sec' % (time.time() - init))
+    return closeness_centrality
+
+def calcul_betweenness_centrality(connection_length_mat, n_node):
+    # input : connection length matrix
+    # output : betweenness centrality of nodes
+    init = time.time()
+    betweenness_centrality = np.zeros((len(connection_length_mat), n_node))
+    for i in range(len(connection_length_mat)):
+        betweenness_centrality[i] = bct.betweenness_wei(connection_length_mat[i])
+    print('time consumed in calculating BC, %f sec' % (time.time() - init))
+    return betweenness_centrality
+
+def calcul_within_module_degree_zscore(input_data, modular_structures, n_node):
+    # input : connectivity matrix, modular structures devided by calcul_module_and_modularity_Louvain
+    # output : within module degree z-score of the nodes
+    init = time.time()
+    within_module_degree_zscore = np.zeros((len(input_data),n_node))
+    for i in range(len(input_data)):
+        within_module_degree_zscore[i] = bct.module_degree_zscore(input_data[i], modular_structures[i])
+    print(f'within module degree z-score calculation is finished, time = {time.time() - init}')
+    return within_module_degree_zscore
+
+def calcul_participation_coefficient(input_data, modular_structures, n_node):
+    # input : connectivity matrix and modular structures
+    # output : participation coefficient
+    init = time.time()
+    participation_coefficient = np.zeros((len(input_data),n_node))
+    for i in range(len(input_data)):
+        participation_coefficient[i] = bct.participation_coef(input_data[i], modular_structures[i])
+    print('participation coef calcultaion time = %f' % (time.time() - init))
+    return participation_coefficient
+
+def calcul_rich_club_coef(input_data, degree):
+    # input : connectivity matrix, degree
+    # output : rich club coefficient, rich club coefficient name list
+    init = time.time()
+    rich_club_coef = np.zeros((len(input_data), int(np.max(degree))))
+    for i in range(len(input_data)):
+        rich_club_coef[i] = bct.rich_club_wu(input_data[i], int(np.max(degree)))
+    rich_club_coef_name_list = []
+    for i in range(int(np.max(degree))):
+        rich_club_coef_name_list.append('rich_club_coef_k=%d'%(i+1))
+    print('time consumed in calculating Rich club, %f sec' % (time.time() - init))
+    return rich_club_coef, rich_club_coef_name_list
---
+++ jungwoos_code/calculate_BNM.py
@@ -0,0 +1,208 @@
+# dMRI의 streamline count data를 input으로 받아서 다양한 network measurement를 구하는 code, pipe line(?)
+# weighted / undirected version, calculation based on brain connectivity toolbox(bct)
+# 구해지는 measurement
+# global measure : average clustering coefficient, global efficiency, modularity
+# nodal measure : degree, strength, clustering coefficient, nodal efficiency, 
+#                 within module degree z-score, participation coefficient, betweenness centrality, k-core index, s-core index
+# rich club measure : rich club coefficient
+
+# streamline count를 network measurement로 변환하는 대략적인 과정에 대한 서술 :
+# streamline count data read --> matrix form으로 변환 --> thresholding --> fragmented subject 제거 --> connectivity matrix scaling
+# --> connection length matrix, distance matrix generation --> network measurement calculation --> file saving
+
+###########################
+# import relavent package #
+###########################
+import sys
+sys.path.append('/usr/local/lib/python3.8/dist-packages/')
+
+import numpy as np
+import pandas as pd
+import bct
+import time
+import math
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+from BNM_calculation_function import *
+
+import argparse
+####`#######################
+
+
+#########################################
+########## calculate BNM ################
+#########################################
+
+# theshold setting
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--th_scheme', required=True, help="input threshold scheme; choose 'density' or 'weight'.")
+parser.add_argument('--th', required=True, help='input threshold value')
+
+args = parser.parse_args()
+
+threshold_scheme = args.th_scheme
+if threshold_scheme == 'weight':
+    threshold = int(args.th)
+elif threshold_scheme == 'density':
+    threshold = float(args.th)
+
+# import structural connectivity data (streamline count data) (outlier filtered)
+count_data_dir = '/scratch/connectome/seojw/BNM analysis/data/con_aparc_count_outlier_filtered.csv'
+count_data = pd.read_csv(count_data_dir).set_index('subjectkey')
+n_node = 84
+
+# node name list
+temporary_list_for_region_list = count_data.columns.str.split('_')
+region_list = []
+region_list.append(temporary_list_for_region_list[0][1])
+for i in range(n_node - 1):
+    region_list.append(temporary_list_for_region_list[i][2])
+
+# connectivity data transform : vector form --> matrix form
+count_mat_data = from_vec_form_to_mat_form(count_data, n_node)
+
+# thresholding : weight-based thresholding or density-based thresholding
+if threshold_scheme == 'weight':
+    thresholded_count_mat_data = weight_based_threshold(count_mat_data, threshold)
+
+    # check n_components & select not fragmented subjects
+    n_comp = calcul_n_comp(thresholded_count_mat_data)
+    not_fragmented_condition = (n_comp == 1)
+    subjectkeys = count_data.index[not_fragmented_condition]
+    count_mat_data = thresholded_count_mat_data[not_fragmented_condition]
+
+elif threshold_scheme == 'density':
+
+    # exclude the subjects having density less than threshold before analysis
+    # to make all analyized subjects  have the same density.
+    density = calcul_density(count_mat_data)
+    inclusion_criteria = (density >= threshold)
+    print(f'the number of excluded subjects is {len(count_data) - sum(inclusion_criteria)}')
+    print(f'remaining subjects is {sum(inclusion_criteria)}')
+    count_mat_data = count_mat_data[inclusion_criteria]
+    subjectkeys = count_data.index[inclusion_criteria]
+
+    thresholded_count_mat_data = density_based_threshold(count_mat_data, threshold)
+
+    # check n_components & select not fragmented subjects
+    n_comp = calcul_n_comp(thresholded_count_mat_data)
+    not_fragmented_condition = (n_comp == 1)
+    subjectkeys = subjectkeys[not_fragmented_condition]
+    count_mat_data = thresholded_count_mat_data[not_fragmented_condition]
+
+# connectivity data 0-1 linear scaling with respect to the max streamline count of the whole dataset
+count_mat_data = count_mat_data / np.max(count_mat_data)
+
+#density_pd = pd.DataFrame(calcul_density(count_mat_data), index=subjectkey, columns=['density'])
+connection_length_mat = calcul_connection_length_mat(count_mat_data)
+distance_mat, NOE_in_SP = calcul_distance_mat(connection_length_mat)
+
+# if you want to use the number of edges in shortest path(NOE_in_SP) for your analysis, un-comment and use below block
+"""
+NOE_in_SP_name_list = []
+for i in range(len(temporary_list_for_region_list)):
+    NOE_in_SP_name_list.append('NOE_in_SP_' + temporary_list_for_region_list[i][1] + '_' + temporary_list_for_region_list[i][2])
+
+NOE_in_SP_line_form = np.zeros(count_data.loc[subjectkeys].shape)
+for i in range(len(count_mat_data)):
+    NOE_in_SP_line_form[i] = from_mat_form_to_vec_form(NOE_in_SP[i])
+NOE_in_SP_pd = pd.DataFrame(NOE_in_SP_line_form, columns=NOE_in_SP_name_list, index=subjectkeys)
+"""
+
+###################################
+###### Brain Network Measures #####
+###################################
+
+# degree
+degree = calcul_degree(count_mat_data, n_node)
+degree_name_list = make_nodal_column_list(region_list, 'deg')
+degree_pd = pd.DataFrame(degree, columns=degree_name_list, index=subjectkeys)
+
+# strength
+strength = calcul_strength(count_mat_data, n_node)
+strength_name_list = make_nodal_column_list(region_list, 'stren')
+strength_pd = pd.DataFrame(strength, columns=strength_name_list, index=subjectkeys)
+
+# global efficiency
+efficiency = calcul_global_efficiency(distance_mat)
+efficiency_pd = pd.DataFrame(efficiency, columns=['E_glob'], index=subjectkeys)
+
+# nodal clustering coefficient & average clustering coefficient
+clustering_coefs = calcul_clust_coef(count_mat_data, n_node)
+clustering_coef_list = make_nodal_column_list(region_list, 'clust_coef')
+clustering_coef_pd = pd.DataFrame(clustering_coefs, columns=clustering_coef_list, index=subjectkeys)
+clustering_coef_pd['avg_clustering_coef'] = clustering_coef_pd.mean(axis=1)
+
+# modular structure & modularity
+modular_structures, modularities = calcul_module_and_modularity_Louvain(count_mat_data, n_node)
+module_name_list = make_nodal_column_list(region_list, 'module_index_of')
+modular_structure_pd = pd.DataFrame(modular_structures, columns=module_name_list, index=subjectkeys)
+module_num_pd = pd.DataFrame(modular_structure_pd.max(axis=1), columns=['module_num'], index=subjectkeys)
+modularity_pd = pd.DataFrame(modularities, columns=['optimal modularity'], index=subjectkeys)
+
+
+####### Measures of Centrality #################
+# : s-core index, k-core index, Closeness Centrality(Cc)=nodal efficiency, Betweenness Centrality(BC),
+# + Within module degree z-score, participation coefficient  #########
+
+# score index
+s_core_index, s_core_size = calcul_s_core(count_mat_data, n_node)
+score_name_list = make_nodal_column_list(region_list, 'score')
+s_core_index_pd = pd.DataFrame(s_core_index, index=subjectkeys, columns=score_name_list)
+s_core_size_pd = pd.DataFrame(s_core_size, index=subjectkeys,
+                              columns=[f'score_size_s={round(i, 2)}' for i in np.arange(0, 1, 0.01)])
+
+# measure of core structure, core-centrality
+# k-coreness (k-core-index, k-core size)
+kcore_index = calcul_k_core(count_mat_data, n_node)
+kcore_index_name_list = make_nodal_column_list(region_list, 'kcore_index_of')
+kcore_index_pd = pd.DataFrame(kcore_index, columns=kcore_index_name_list, index=subjectkeys)
+
+# closeness centrality : Cc = nodal efficiency
+# (Ref. Rubinov & Sporns 2010, Complex network measures of brain connectivity:Uses and interpretations)
+closeness_centrality = calcul_closeness_centrality(distance_mat, n_node)
+closeness_centrality_list = make_nodal_column_list(region_list, 'Cc')
+closeness_centrality_pd = pd.DataFrame(closeness_centrality, columns=closeness_centrality_list, index=subjectkeys)
+
+# betweenness centrality : BC
+betweenness_centrality = calcul_betweenness_centrality(connection_length_mat, n_node)
+betweenness_centrality_list = make_nodal_column_list(region_list, 'BC')
+betweenness_centrality_pd = pd.DataFrame(betweenness_centrality, columns=betweenness_centrality_list, index=subjectkeys)
+
+# within module degree z-score
+within_module_degree_zscore = calcul_within_module_degree_zscore(count_mat_data, modular_structures, n_node)
+within_module_degree_list = make_nodal_column_list(region_list, 'within_module_deg')
+within_module_degree_pd = pd.DataFrame(within_module_degree_zscore, columns=within_module_degree_list, index=subjectkeys)
+
+# participation coeffcient
+participation_coefficient = calcul_participation_coefficient(count_mat_data, modular_structures, n_node)
+participation_coef_list = make_nodal_column_list(region_list, 'participation_coef')
+participation_coefficient_pd = pd.DataFrame(participation_coefficient, columns=participation_coef_list, index=subjectkeys)
+
+# rich club coefficient
+rich_club_coef, rich_club_coef_name_list = calcul_rich_club_coef(count_mat_data, degree)
+rich_club_coef_pd = pd.DataFrame(rich_club_coef, columns=rich_club_coef_name_list, index=subjectkeys).fillna(0)
+
+####################################
+####################################
+
+
+####################################
+######### save BNM data ############
+####################################
+
+BNM = pd.concat([degree_pd, strength_pd, efficiency_pd, clustering_coef_pd, modularity_pd, s_core_index_pd, kcore_index_pd,
+               closeness_centrality_pd, betweenness_centrality_pd, within_module_degree_pd, participation_coefficient_pd,
+                rich_club_coef_pd], axis=1)
+scaler = StandardScaler()
+BNM_zscaled = pd.DataFrame(scaler.fit_transform(BNM), columns=BNM.columns, index=BNM.index)
+
+if threshold_scheme == 'weight':
+    file_name_th = 'ab'
+elif threshold_scheme == 'density':
+    file_name_th = 'den'
+
+BNM.to_csv('/scratch/connectome/seojw/BNM analysis/data/BNM_'+file_name_th+str(threshold)+'.csv')
+BNM_zscaled.to_csv('/scratch/connectome/seojw/BNM analysis/data/BNM_'+file_name_th+str(threshold)+'_zscaled.csv')
