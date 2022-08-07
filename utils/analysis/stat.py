f7a925c03c89f0842e0597459f7002dd15db988a
dyhan316 <dyhan0316@gmail.com>
2022-08-07 15:49:55 +0900

    added covariant option to the group_corr method in stat.py

--- utils/analysis/stat.py
+++ utils/analysis/stat.py
@@ -7,15 +7,24 @@ import numpy as np
 print("IMPORTED")
 
 
-def grouped_corr(data, y, p = 0.95, filter_bonf = True, n = None ):
+def grouped_corr(data, y, p = 0.95, filter_bonf = True, n = None, covar = None ):
     """description about this method is long, so it is put after the function definition below"""
     corr_result = pd.DataFrame()
-    merged_data = pd.concat([data, y], join='inner', axis=1) 
+    
+    if covar is not None:
+        merged_data = pd.concat([data, y, covar], join='inner', axis=1) 
+    else :
+        merged_data = pd.concat([data, y], join='inner', axis=1) 
             
     for measure in data.columns:
-        corr_result = pd.concat([corr_result, pd.DataFrame(pg.corr(merged_data[measure], merged_data[y.name]))], axis=0)
+        if covar is not None:
+            corr_result = pd.concat([corr_result, pd.DataFrame(pg.partial_corr(data = merged_data, x = measure, y = y.name, covar = [var for var in covar.columns]))])#, axis = 0)
+        else:
+            corr_result = pd.concat([corr_result, pd.DataFrame(pg.corr(merged_data[measure], merged_data[y.name]))], axis=0)
+                                     
     corr_result.index = data.columns
     
+    #filter significant things only
     if filter_bonf == True :
         #if no custom n value is specified, we infer n from the data column갯수
         if n == None:
@@ -39,6 +48,8 @@ def grouped_corr(data, y, p = 0.95, filter_bonf = True, n = None ):
             * filter_bonf : True면 output이 bonfernnoi corrected p value보다 더 significant한것들만 output
             * n : custom # of cases (원래는 자동으로 갯수를 새주나, 직접 넣어줄 수도 있다)
                 * 즉, filter_bonf = True면, p_value < p/n 인 아이들만 살림
+        * covar :
+            * if covariance dataframe is provided, will use it to run covariances into account
 
     OUTPUT : data의 columns와 y 의 correlation 결과물들 (r, p value and so on) (possibly filtered by bonferrnoi corrected p value), SORTED by r value
     """
